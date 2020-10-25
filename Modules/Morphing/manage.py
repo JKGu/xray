c11ec769e02552f32c54e779e29307e6a95ee5a0
Steven <jgu8@u.rochester.edu>
2020-10-25 00:06:50 -0400

    morph module completed

--- Modules/Morphing/manage.py
+++ Modules/Morphing/manage.py
@@ -1,8 +1,7 @@
-import os
-import shutil
-import pathlib
+import os, shutil, pathlib, PIL, sys
 import matplotlib.image as mpimg 
 import matplotlib.pyplot as plt 
+
 def __create_folder_if_not_exist(str):
     if not os.path.isdir(str):
         try:
@@ -11,50 +10,84 @@ def __create_folder_if_not_exist(str):
             print ('Failed to create '+ str +"\n")
 
 __MYDIR = str(pathlib.Path(__file__).parent.absolute())
+__USER = __MYDIR+ '/Morph/UserFiles/'
+__INPUT = __MYDIR+'/Morph/UserFiles/InputImages/'
+__OUTPUT = __MYDIR+'/Morph/UserFiles/OutputImages/'
+__MASK = __MYDIR+'/Morph/UserFiles/Masks/'
 
-def build_wrkdir():
-    __create_folder_if_not_exist(__MYDIR+ '/MorphLib/UserFiles/')
-    __create_folder_if_not_exist(__MYDIR+ '/MorphLib/UserFiles/InputImages/')
-    __create_folder_if_not_exist(__MYDIR+ '/MorphLib/UserFiles/OutputImages/')
-    __create_folder_if_not_exist(__MYDIR+ '/MorphLib/UserFiles/Masks/')
-    __create_folder_if_not_exist(__MYDIR+ '/MorphLib/UserFiles/tmp/')
-    __create_folder_if_not_exist(__MYDIR+ '/MorphLib/UserFiles/tmp/tmp0/')
-    __create_folder_if_not_exist(__MYDIR+ '/MorphLib/UserFiles/tmp/tmp1/')
+def __cleanup_folder(folder):
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
 
-def cleanup_tmpfolder():
-    shutil.rmtree(__MYDIR+ '/MorphLib/UserFiles/tmp/')
 
-def cleanup_wrkdir():
-    cleanup_tmpfolder()
-    shutil.rmtree(__MYDIR+ '/MorphLib/UserFiles/InputImages/')
-    shutil.rmtree(__MYDIR+ '/MorphLib/UserFiles/OutputImages/')
-    shutil.rmtree(__MYDIR+ '/MorphLib/UserFiles/Masks/')
+def __build_wrkdir():
+    __create_folder_if_not_exist(__USER)
+    __create_folder_if_not_exist(__INPUT)
+    __create_folder_if_not_exist(__MASK)
+    __create_folder_if_not_exist(__OUTPUT)
 
-def init_wrkdir(
-    inputImagePath=__MYDIR+'/MorphLib/UserFiles/InputImages/',
-    maskPath=__MYDIR+ '/MorphLib/UserFiles/Masks/'
-    ):
-    build_wrkdir()
-    cleanup_tmpfolder()
-    shutil.rmtree(__MYDIR+ '/MorphLib/UserFiles/OutputImages/')
-    
-    if inputImagePath != __MYDIR+'/MorphLib/UserFiles/InputImages/':
-        images = os.listdir(inputImagePath)
-        for file in images:
-            if file.endswith('.png'):
-                full_file_name = os.path.join(inputImagePath, file)
-                shutil.copy(full_file_name,__MYDIR+ '/MorphLib/UserFiles/InputImages/')
-    
-    if maskPath != __MYDIR+ '/MorphLib/UserFiles/Masks/':
-        masks = os.listdir(maskPath)
-        for file in masks:
-            if file.endswith('.png'):
-                full_file_name = os.path.join(maskPath, file)
-                shutil.copy(full_file_name,__MYDIR+ '/MorphLib/UserFiles/Masks/')
 
-def run_morph_exe():
+def __cleanup_wrkdir():
+    __cleanup_folder(__INPUT)
+    __cleanup_folder(__OUTPUT)
+    __cleanup_folder(__MASK)
+
+def __copy_all_files(dir_src,dir_dst,suffix):
+    files = os.listdir(dir_src)
+    for file in files:
+        if file.endswith(suffix):
+            full_file_name = os.path.join(dir_src, file)
+            shutil.copy(full_file_name,dir_dst)
+
+
+def build_new_project(inputImagePath, customMaskPath = None):
+
+    print('Building working directories...')
+    __build_wrkdir()
+    __cleanup_wrkdir()
+
+    print('Loading images...')
+    __copy_all_files(inputImagePath, __INPUT, '.png')
+    inputs = os.listdir(__INPUT)
+
+    if len(inputs) <= 1:
+        print("WARNING: Insufficient images. Abort.")
+        return
+
+    print('Preparing the mask image...')
+    if customMaskPath != None:
+        print('Importing custom mask...')
+        shutil.copy(customMaskPath,__MASK)
+        os.rename(__MASK+os.listdir(__MASK)[0], __MASK+'mask.png')
+        
+    else:
+        print('Generating an empty mask...')
+        sampleName = inputs[0]
+        image = PIL.Image.open(__INPUT+sampleName)
+        width, height = image.size
+        image = PIL.Image.new('RGB', (width,height))
+        image.save(__MASK+"mask.png", "PNG")
+
+    print('Validating image size...')
+    standard = PIL.Image.open(__MASK+'mask.png')
+    width, height = standard.size
+    for img in inputs:
+        image = PIL.Image.open(__INPUT+img)
+        new_image = image.resize((width, height))
+        new_image.save(__INPUT+img)
+
+    print('New project successfully created.')
+
+def start_project():
     dir_tmp = os.getcwd()
-    os.chdir(__MYDIR+"\Morph\src")
+    os.chdir(__MYDIR+"\Morph")
     print("DIR changed to ",os.getcwd())
     print("Executing Example.exe...")
     os.system("Example.exe")
@@ -63,13 +96,11 @@ def run_morph_exe():
     print("DIR changed back to ",os.getcwd())
     print("Result image:")
     viewOutputImage()
-    cleanup_tmpfolder()
+
 
 def viewOutputImage():
-    img = mpimg.imread(__MYDIR+'/MorphLib/UserFiles/OutputImages/output.png') 
+    img = mpimg.imread(__OUTPUT+'output.png') 
     plt.imshow(img) 
 
 def getOutputImage(outputPath):
-    shutil.copy(__MYDIR+'/MorphLib/UserFiles/OutputImages/output.png', outputPath)
-    img = mpimg.imread(__MYDIR+'/MorphLib/UserFiles/OutputImages/output.png') 
-    plt.imshow(img) 
+    shutil.copy(__OUTPUT+'output.png', outputPath)
